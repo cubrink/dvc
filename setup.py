24c4a879d2288e426f22d936478fdde506a6c83c
tmay <tmay@hunter.com>
2021-06-18 11:03:55 -0500

    Use fixed version of doltcli

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ install_requires = [
     "typing_extensions>=3.7.4",
     "fsspec @ git+https://github.com/intake/filesystem_spec.git",
     "diskcache>=5.2.1",
-    "doltcli>=0.1.4",
+    "doltcli @ git+https://github.com/TannerAMay/doltcli.git@max/windows-tmpfile",
 ]
 
 
