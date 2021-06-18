5cbcb7446ba3c6851a39650ff221a024778c4bfd
tmay <tmay@hunter.com>
2021-06-18 12:21:54 -0500

    dvc push and dvc pull work Use new version of push and fetch

--- dvc/objects/stage.py
+++ dvc/objects/stage.py
@@ -204,9 +204,10 @@ def get_dolt_hash(path_info, fs, name, odb, state, **kwargs):
     dir_info = _collect_dir(path_info, fs, name, state, **kwargs)
 
     hash_info.size = dir_info.size
-    hash_info.dir_info = dir_info
 
-    return hash_info
+    hashFile = HashFile(path_info, fs, hash_info, name)
+
+    return hashFile
 
 def _get_tree_obj(path_info, fs, name, odb, state, upload, **kwargs):
     from .tree import Tree
