4f5fcf4939ed1e970c848b4b88f3a7beda26d75f
Curtis Brinker <curtisbrnkr@gmail.com>
2021-06-22 08:39:59 -0500

    Updated doltcli dependency

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ install_requires = [
     "typing_extensions>=3.7.4",
     "fsspec>=2021.6.1",
     "diskcache>=5.2.1",
-    "doltcli @ git+https://github.com/TannerAMay/doltcli.git@max/windows-tmpfile",
+    "doltcli @ git+https://github.com/dolthub/doltcli.git",
 ]
 
 
